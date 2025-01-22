8c9eaa721eb96d9c6ac55ffe388eb466d4d99d2b
Laila Khan <lailatariqkhan@gmail.com>
2025-01-22 15:33:54 +0000

    Fetch API and Extract require data (Destination, Route No. and Time to Station (seconds)).

--- testFetch.js
+++ testFetch.js
@@ -1,5 +1,22 @@
 import fetch from 'node-fetch';
 
+//Fetch TFL Stop-Point API. Note at this stage we are taking URL as it is with Stop Code. We will change it later to user prompt.
+async function fetchAPI()
+{
 const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
+let data = await response.json();
+await extractData(data);
+}
 
-console.log(await response.json());
+// Extract required data from API (Destination,Route Number,TimetoStation(seconds))
+async function extractData(data) {
+    const extractInfo = data.map(busDetails=>({
+        Destination:busDetails.destinationName, 
+        Route:busDetails.lineName,
+        TimeToStation:busDetails.timeToStation
+    }));
+    console.log(extractInfo);
+}
+
+// Calling FetchAPI
+fetchAPI();
