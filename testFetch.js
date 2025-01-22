accc0331a5ac36c2981ebb951ffd634c7b9419b7
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-22 12:35:22 +0000

    Added 'await response.json()' to read Json response body

--- testFetch.js
+++ testFetch.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
 
 const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
-console.log(response);
+
+console.log(await response.json());
