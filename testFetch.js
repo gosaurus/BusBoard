b995ec3e5c13b6e89b93cd4c5f576c226e90c939
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-23 12:00:49 +0000

    Part 1 completed

--- testFetch.js
+++ testFetch.js
@@ -1,29 +1,70 @@
 import fetch from 'node-fetch';
+import readline from 'readline-sync';
 
-//Fetch TFL Stop-Point API. Note at this stage we are taking URL as it is with Stop Code. We will change it later to user prompt.
-async function fetchAPI()
-{
-const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
-let data = await response.json();
-await extractData(data);
+function getStopCodeFromUser() {
+    const stopcode = readline.question("Please enter the required stop code: ");
+    const regex = /\b[0-9]{9}[A-Z]{1}\b/;
+    if(regex.test(stopcode)){
+        return stopcode;
+    }
+    else{
+        console.log("Invalid response");
+        getStopCodeFromUser();
+    }
 }
 
-// Extract required data from API (Destination,Route Number,TimetoStation(seconds)); Returns list of objects 
-async function extractData(data) {
-    const extractInfo = data.map(busDetails=>({
+function getURL(stopcode){  
+    return "https://api.tfl.gov.uk/StopPoint/"+stopcode+"/Arrivals";
+}
+
+
+async function fetchAPI(apiUrl) {
+    const response = await fetch(apiUrl);
+    return await response.json();
+}
+
+async function parseData(data) {
+    return data.map(busDetails => ({
         Destination:busDetails.destinationName, 
         Route:busDetails.lineName,
         TimeToStation:busDetails.timeToStation
     }));
-    // console.log(extractInfo);
-    return extractInfo
 }
 
-// Sort buses by timeToStation 
-function arrivalTime(extractInfo) {
-    let sortedBuses = extractInfo.sort((a,b) = a.busDetails.timeToStation - b.busDetails.timeToStation);
+function sortByArrivalTime(busInfo) {
+    return busInfo.sort((a,b) => a.TimeToStation - b.TimeToStation);
 }
 
-// Calling FetchAPI
+function convertArrivalTimeToMinutes(sortedBusArrival){
+    sortedBusArrival.forEach(bus=> bus.TimeToStation=Math.ceil(bus.TimeToStation/60));
+    return sortedBusArrival;
+}
 
-fetchAPI();
+function getFirstFiveBuses(busArrivalInMinutes) {
+    return busArrivalInMinutes.slice(0,5);
+}
+
+function formattedBusDetails(firstFiveBuses){
+    firstFiveBuses.forEach((bus,index)=>
+{
+    console.log(`Bus ${index+1}`);
+    for(let[key,value] of Object.entries(bus)) {
+        if(key==="TimeToStation") {
+            console.log(`${key} : ${value} minutes`);
+        }
+        else {
+        console.log(`${key} : ${value}`);
+        }
+    }
+});
+}
+
+// Main 
+const stopCode = getStopCodeFromUser();
+const apiURL = getURL(stopCode);
+const data = await fetchAPI(apiURL);
+const busInfo = await parseData(data);
+const sortedBusArrival = sortByArrivalTime(busInfo);
+const busArrivalInMinutes = convertArrivalTimeToMinutes(sortedBusArrival);
+const firstFiveBuses = getFirstFiveBuses (busArrivalInMinutes);
+formattedBusDetails(firstFiveBuses);
